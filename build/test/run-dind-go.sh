45f2301e7009da4884f179895165d02ddcde3375
Martin Weindel <martin.weindel@sap.com>
2019-05-02 14:47:02 +0200

    run dind: remove obsolete privileged flag

--- build/test/run-dind-go.sh
+++ build/test/run-dind-go.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --privileged -it --rm -v /var/run/docker.sock:/var/run/docker.sock  dind-go bash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock dind-go bash
