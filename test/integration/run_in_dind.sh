f3c27201be7f74cb28b56681c4ff159c32a47e60
Raphael Vogel <raphael.vogel@sap.com>
2019-05-07 15:50:31 +0200

    add sleep

--- test/integration/run_in_dind.sh
+++ test/integration/run_in_dind.sh
@@ -8,5 +8,7 @@ ROOTDIR=$(realpath "$SCRIPT_BASEDIR/../..")
 # start Docker daemon
 /usr/local/bin/dockerd-entrypoint.sh &
 
+sleep 10
+
 # run tests
 $SCRIPT_BASEDIR/run.sh -k -- -v -focus Provider_Secret
