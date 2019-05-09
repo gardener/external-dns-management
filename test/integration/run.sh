ac6a1d1fdd4590682ba7c35581d2f478fdce2d9e
Martin Weindel <martin.weindel@sap.com>
2019-05-09 16:54:48 +0200

    using gke cluster

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Copyright 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
@@ -45,7 +59,7 @@ while [ "$1" != "" ]; do
     esac
 done
 
-if [ "$CLUSTER" == "" ]; then
+if [ "$EXTERNAL" == "" ]; then
   docker version > /dev/null || (echo "Local Docker installation needed" && exit 1)
 fi
 
@@ -53,7 +67,7 @@ if [ "$VERBOSE" != "" ]; then
   set -x
 fi
 
-if [ "$NOBOOTSTRAP" == "" ] && [ "$CLUSTER" == "" ]; then
+if [ "$NOBOOTSTRAP" == "" ] && [ "$EXTERNAL" == "" ]; then
   echo Starting Kubernetes IN Docker...
 
   # prepare Kubernetes IN Docker - local clusters for testing Kubernetes
@@ -66,14 +80,14 @@ if [ "$NOBOOTSTRAP" == "" ] && [ "$CLUSTER" == "" ]; then
   kind create cluster --name integration 
 fi
 
-if [ "$CLUSTER" != "" ]; then
+if [ "$EXTERNAL" != "" ]; then
   echo using GKE cluster
 
-  gcloud container clusters get-credentials $CLUSTER
+  gcloud container clusters get-credentials $GKE_CLUSTER --project $GKE_PROJECT --zone $GKE_ZONE
 
-  kubectl config view --minify=true --raw > /tmp/kubeconfig-$CLUSTER.yaml
+  kubectl config view --minify=true --raw > /tmp/kubeconfig-gke.yaml
   # set KUBECONFIG
-  export KUBECONFIG=/tmp/kubeconfig-$CLUSTER.yaml
+  export KUBECONFIG=/tmp/kubeconfig-gke.yaml
 else
   # set KUBECONFIG
   export KUBECONFIG=$(kind get kubeconfig-path --name="integration")
@@ -88,7 +102,7 @@ which ginkgo || go install github.com/onsi/ginkgo/ginkgo
 cd $ROOTDIR/test/integration && ginkgo -failFast "$@" ; cd -
 
 # cleanup
-if [ "$KEEP_CLUSTER" == "" ] && [ "$CLUSTER" == "" ]; then
+if [ "$KEEP_CLUSTER" == "" ] && [ "$EXTERNAL" == "" ]; then
   unset KUBECONFIG
   kind delete cluster --name integration
 fi
