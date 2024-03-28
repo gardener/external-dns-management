4414d62069d5faafa7d8348437b969e734f48f86
Martin Weindel <martin.weindel@sap.com>
2024-03-28 13:20:16 +0100

    remove obsolete controller registration (#362)

--- charts/external-dns-management/doc.go
+++ charts/external-dns-management/doc.go
@@ -2,7 +2,5 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate ../../hack/generate-controller-registration.sh dns-external ../../charts/external-dns-management/ ../../VERSION ../../examples/controller-registration.yaml         DNSProvider:aws-route53 DNSProvider:alicloud-dns DNSProvider:azure-dns DNSProvider:azure-private-dns DNSProvider:google-clouddns DNSProvider:openstack-designate DNSProvider:cloudflare-dns DNSProvider:netlify-dns DNSProvider:infoblox-dns DNSProvider:remote
-
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
