2855090689b0c3a2de7c62764eb9b1dd9fd8e28b
Martin Weindel <martin.weindel@sap.com>
2025-01-22 10:53:27 +0100

    ignore TTL on comparing alias records (#411)

--- pkg/dns/records_test.go
+++ pkg/dns/records_test.go
@@ -26,6 +26,10 @@ func TestMatch(t *testing.T) {
 		{RecordSet{Type: RS_TXT, TTL: 600, Records: []*Record{{"\"foo\""}}}, RecordSet{Type: RS_TXT, TTL: 600, Records: []*Record{{"\"foo\""}, {"\"foo\""}}}, false},
 		// different type = not equal
 		{RecordSet{Type: RS_A, TTL: 600, Records: []*Record{{"1.2.3.4"}}}, RecordSet{Type: RS_TXT, TTL: 600, Records: []*Record{{"\"foo\""}}}, false},
+		// alias type: ignore TTL as ignored in AWS Route53 anyway
+		{RecordSet{Type: RS_ALIAS_A, TTL: 60, Records: []*Record{{"foo.example.com"}}}, RecordSet{Type: RS_ALIAS_A, TTL: 0, Records: []*Record{{"foo.example.com"}}}, true},
+		// alias_a type: ignore TTL as ignored in AWS Route53 anyway
+		{RecordSet{Type: RS_ALIAS_AAAA, TTL: 60, Records: []*Record{{"foo.example.com"}}}, RecordSet{Type: RS_ALIAS_AAAA, TTL: 0, Records: []*Record{{"foo.example.com"}}}, true},
 	}
 
 	for _, entry := range table {
