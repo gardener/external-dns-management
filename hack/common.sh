11e86079181da95c1b0d9dd666319f2b726f554b
Marc Vornetran <marc1404@users.noreply.github.com>
2025-06-23 13:58:25 +0200

    chore: Remove year from copyright headers (#523)

--- hack/common.sh
+++ hack/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+# SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
 #
 # SPDX-License-Identifier: Apache-2.0
 set -e
