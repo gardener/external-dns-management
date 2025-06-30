09d7c807e2be34937584f32ecbba07a6ec241796
Martin Weindel <martin.weindel@sap.com>
2025-06-30 14:51:08 +0200

    update controller-manager-library

--- pkg/client/dns/informers/externalversions/dns/v1alpha1/dnsprovider.go
+++ pkg/client/dns/informers/externalversions/dns/v1alpha1/dnsprovider.go
@@ -49,13 +49,25 @@ func NewFilteredDNSProviderInformer(client versioned.Interface, namespace string
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.DnsV1alpha1().DNSProviders(namespace).List(context.TODO(), options)
+				return client.DnsV1alpha1().DNSProviders(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.DnsV1alpha1().DNSProviders(namespace).Watch(context.TODO(), options)
+				return client.DnsV1alpha1().DNSProviders(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.DnsV1alpha1().DNSProviders(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.DnsV1alpha1().DNSProviders(namespace).Watch(ctx, options)
 			},
 		},
 		&apisdnsv1alpha1.DNSProvider{},
