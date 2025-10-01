b207e51d805c441821caa1502fd2ce9d88cbb802
Martin Weindel <martin.weindel@sap.com>
2025-10-01 13:46:01 +0200

    [Controller-Runtime-Rewrite] Add handler for `netlify-dns` (#643)
    * [Controller-Runtime-Rewrite] Add handler for `netlify-dns`
    
    * address review feedback

--- pkg/dnsman2/dns/provider/handler/netlify/access.go
+++ pkg/dnsman2/dns/provider/handler/netlify/access.go
@@ -14,11 +14,12 @@ import (
 	"github.com/netlify/open-api/go/plumbing/operations"
 	"k8s.io/client-go/util/flowcontrol"
 
-	"github.com/gardener/external-dns-management/pkg/dns/provider"
-	"github.com/gardener/external-dns-management/pkg/dns/provider/raw"
+	"github.com/gardener/external-dns-management/pkg/dnsman2/dns"
+	"github.com/gardener/external-dns-management/pkg/dnsman2/dns/provider"
+	"github.com/gardener/external-dns-management/pkg/dnsman2/dns/provider/raw"
 )
 
-type Access interface {
+type accessor interface {
 	ListZones(consume func(zone models.DNSZone) (bool, error)) error
 	ListRecords(zoneId string, consume func(record models.DNSRecord) (bool, error)) error
 
@@ -32,7 +33,7 @@ type access struct {
 	rateLimiter flowcontrol.RateLimiter
 }
 
-func ClientCredentials(apiToken string) runtime.ClientAuthInfoWriter {
+func clientCredentials(apiToken string) runtime.ClientAuthInfoWriter {
 	return runtime.ClientAuthInfoWriterFunc(func(r runtime.ClientRequest, _ strfmt.Registry) error {
 		_ = r.SetHeaderParam("User-Agent", "gardener-external-dns-management")
 		_ = r.SetHeaderParam("Authorization", "Bearer "+apiToken)
@@ -40,14 +41,14 @@ func ClientCredentials(apiToken string) runtime.ClientAuthInfoWriter {
 	})
 }
 
-func NewAccess(apiToken string, metrics provider.Metrics, rateLimiter flowcontrol.RateLimiter) (Access, error) {
+func newAccess(apiToken string, metrics provider.Metrics, rateLimiter flowcontrol.RateLimiter) (accessor, error) {
 	client := plumbing.Default.Operations
-	clientCredentials := ClientCredentials(apiToken)
+	clientCredentials := clientCredentials(apiToken)
 	return &access{client: client, authInfo: clientCredentials, metrics: metrics, rateLimiter: rateLimiter}, nil
 }
 
 func (this *access) ListZones(consume func(zone models.DNSZone) (bool, error)) error {
-	this.metrics.AddGenericRequests(provider.M_LISTZONES, 1)
+	this.metrics.AddGenericRequests(provider.MetricsRequestTypeListZones, 1)
 	this.rateLimiter.Accept()
 	results, err := this.client.GetDNSZones(nil, this.authInfo)
 	if err != nil {
@@ -62,7 +63,7 @@ func (this *access) ListZones(consume func(zone models.DNSZone) (bool, error)) e
 }
 
 func (this *access) ListRecords(zoneID string, consume func(record models.DNSRecord) (bool, error)) error {
-	this.metrics.AddZoneRequests(zoneID, provider.M_LISTRECORDS, 1)
+	this.metrics.AddZoneRequests(zoneID, provider.MetricsRequestTypeListRecords, 1)
 	this.rateLimiter.Accept()
 	params := operations.NewGetDNSRecordsParams()
 	params.ZoneID = zoneID
@@ -81,14 +82,14 @@ func (this *access) ListRecords(zoneID string, consume func(record models.DNSRec
 func (this *access) CreateRecord(_ context.Context, r raw.Record, zone provider.DNSHostedZone) error {
 	a := r.(*Record)
 	ttl := r.GetTTL()
-	testTTL(&ttl)
+	clampTTL(&ttl)
 	dnsRecord := models.DNSRecordCreate{
 		Type:     r.GetType(),
 		Hostname: r.GetDNSName(),
 		Value:    r.GetValue(),
 		TTL:      ttl,
 	}
-	this.metrics.AddZoneRequests(zone.Id().ID, provider.M_CREATERECORDS, 1)
+	this.metrics.AddZoneRequests(zone.ZoneID().ID, provider.MetricsRequestTypeCreateRecords, 1)
 	this.rateLimiter.Accept()
 	createParams := operations.NewCreateDNSRecordParams()
 	createParams.SetZoneID(a.DNSZoneID)
@@ -100,8 +101,7 @@ func (this *access) CreateRecord(_ context.Context, r raw.Record, zone provider.
 func (this *access) UpdateRecord(ctx context.Context, r raw.Record, zone provider.DNSHostedZone) error {
 	// Netlify does not support updating a record
 	// Delete the existing record and re-create it
-	err := this.DeleteRecord(ctx, r, zone)
-	if err != nil {
+	if err := this.DeleteRecord(ctx, r, zone); err != nil {
 		return err
 	}
 	return this.CreateRecord(ctx, r, zone)
@@ -112,7 +112,7 @@ func (this *access) DeleteRecord(_ context.Context, r raw.Record, zone provider.
 	deleteParams := operations.NewDeleteDNSRecordParams()
 	deleteParams.SetZoneID(a.DNSZoneID)
 	deleteParams.SetDNSRecordID(a.ID)
-	this.metrics.AddZoneRequests(zone.Id().ID, provider.M_DELETERECORDS, 1)
+	this.metrics.AddZoneRequests(zone.ZoneID().ID, provider.MetricsRequestTypeDeleteRecords, 1)
 	this.rateLimiter.Accept()
 	_, err := this.client.DeleteDNSRecord(deleteParams, this.authInfo)
 	return err
@@ -124,29 +124,29 @@ func (this *access) NewRecord(fqdn, rtype, value string, zone provider.DNSHosted
 		Hostname:  fqdn,
 		Value:     value,
 		TTL:       int64(ttl),
-		DNSZoneID: zone.Id().ID,
+		DNSZoneID: zone.ZoneID().ID,
 	})
 }
 
-func (this *access) GetRecordSet(_ context.Context, dnsName, rtype string, zone provider.DNSHostedZone) (raw.RecordSet, error) {
-	rs := raw.RecordSet{}
+func (this *access) GetRecordList(_ context.Context, dnsName, rtype string, zone provider.DNSHostedZone) (raw.RecordList, []*dns.RoutingPolicy, error) {
+	rl := raw.RecordList{}
 	consume := func(record models.DNSRecord) (bool, error) {
 		a := (*Record)(&record)
 		if a.Type == rtype && a.Hostname == dnsName {
-			rs = append(rs, a)
+			rl = append(rl, a)
 		}
 		return true, nil
 	}
 
 	// no filtering provided by API, we have to list complete zone and filter
-	err := this.ListRecords(zone.Id().ID, consume)
+	err := this.ListRecords(zone.ZoneID().ID, consume)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return rs, nil
+	return rl, nil, nil
 }
 
-func testTTL(ttl *int64) {
+func clampTTL(ttl *int64) {
 	if *ttl < 1 {
 		*ttl = 1
 	}
