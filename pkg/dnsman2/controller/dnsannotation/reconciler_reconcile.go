c6b756a7cd68c66b4085c82639300843f363e5ae
Martin Weindel <martin.weindel@sap.com>
2025-10-22 15:49:11 +0200

    Implementation `DNSAnnotation` controller

---
+++ pkg/dnsman2/controller/dnsannotation/reconciler_reconcile.go
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package dnsanntation
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/gardener/external-dns-management/pkg/apis/dns/v1alpha1"
+)
+
+func (r *Reconciler) reconcile(ctx context.Context, log logr.Logger, annotation *v1alpha1.DNSAnnotation) (reconcile.Result, error) {
+	if err := r.state.SetResourceAnnotations(annotation.Spec.ResourceRef, client.ObjectKeyFromObject(annotation), annotation.Spec.Annotations); err != nil {
+		log.Info("failed to set resource annotations in state", "error", err.Error())
+		return reconcile.Result{}, r.updateStatus(ctx, annotation, func(status *v1alpha1.DNSAnnotationStatus) error {
+			status.Message = err.Error()
+			status.Active = false
+			return nil
+		})
+	}
+
+	return reconcile.Result{}, r.updateStatus(ctx, annotation, func(status *v1alpha1.DNSAnnotationStatus) error {
+		_, message, active := r.state.GetResourceAnnotationStatus(annotation.Spec.ResourceRef)
+		if message != status.Message || active != status.Active {
+			log.V(1).Info("updated annotation status", "active", active, "message", message)
+		}
+		status.Message = message
+		status.Active = active
+		return nil
+	})
+}
