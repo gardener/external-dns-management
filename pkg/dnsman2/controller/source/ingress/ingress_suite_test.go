d0cc0dcca5db8e6efde2c1801b4497f5793a36de
Marc Vornetran <marc.vornetran@sap.com>
2025-12-03 18:10:52 +0100

    test: Prepare test suite

---
+++ pkg/dnsman2/controller/source/ingress/ingress_suite_test.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package ingress_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestLandscape(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Ingress Source Controller Suite")
+}
