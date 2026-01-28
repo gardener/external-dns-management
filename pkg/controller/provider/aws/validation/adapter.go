7b4fce48381752e730f7f539c5b6741fbd591809
Martin Weindel <martin.weindel@sap.com>
2026-01-28 15:22:44 +0100

    [aws-route53] Mention `region` alias for `AWS_REGION` field; allow `sessionToken` as alias for `AWS_SESSION_TOKEN`

--- pkg/controller/provider/aws/validation/adapter.go
+++ pkg/controller/provider/aws/validation/adapter.go
@@ -41,7 +41,7 @@ func NewAdapter() provider.DNSHandlerAdapter {
 	checks.Add(provider.OptionalProperty("AWS_USE_CREDENTIALS_CHAIN").
 		RequiredIfUnset("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY").
 		Validators(provider.NoTrailingWhitespaceValidator, provider.BoolValidator))
-	checks.Add(provider.OptionalProperty("AWS_SESSION_TOKEN").
+	checks.Add(provider.OptionalProperty("AWS_SESSION_TOKEN", "sessionToken").
 		Validators(provider.MaxLengthValidator(512)).
 		HideValue())
 	return &adapter{checks: checks}
