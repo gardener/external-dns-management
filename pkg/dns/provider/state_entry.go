de0d169f7d1a559cbd2e1a5b7bd53dcf5c7f7eec
Martin Weindel <martin.weindel@sap.com>
2026-02-27 14:46:49 +0100

    Exclude ignored entries in change model updates

--- pkg/dns/provider/state_entry.go
+++ pkg/dns/provider/state_entry.go
@@ -259,6 +259,11 @@ func (this *state) HandleUpdateEntry(logger logger.LogContext, op string, object
 	}
 
 	if ignored, ignoredForDeletion, annotation := ignoredByAnnotation(object); ignored {
+		e := this.entries[object.ObjectName()]
+		if e != nil && e.object != nil {
+			// update annotations in case they were changed (needed for ChangeModel to ignore the entry for updates)
+			e.object.SetAnnotations(object.GetAnnotations())
+		}
 		if old != nil {
 			old.ignoredForDeletion = ignoredForDeletion
 		}
