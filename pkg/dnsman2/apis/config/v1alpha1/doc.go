2316127b713d1d23e80b9ba5088a5ab04932b3ee
Martin Weindel <martin.weindel@sap.com>
2026-05-06 16:19:41 +0200

    Move api-references to docs folder and fix forwarding links

--- pkg/dnsman2/apis/config/v1alpha1/doc.go
+++ pkg/dnsman2/apis/config/v1alpha1/doc.go
@@ -7,7 +7,7 @@
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
 
-//go:generate crd-ref-docs --source-path=. --config=../../../../../hack/api-reference/config.yaml --renderer=markdown --templates-dir=$GARDENER_HACK_DIR/api-reference/template --log-level=ERROR --output-path=../../../../../hack/api-reference/config.md
+//go:generate crd-ref-docs --source-path=. --config=../../../../../hack/api-reference/config.yaml --renderer=markdown --templates-dir=$GARDENER_HACK_DIR/api-reference/template --log-level=ERROR --output-path=../../../../../docs/api-reference/config.md
 
 // +groupName=config.dns.gardener.cloud
 package v1alpha1 // import "github.com/gardener/external-dns-management/pkg/dnsman2/apis/config/v1alpha1"
